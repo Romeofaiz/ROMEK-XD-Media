3b3a2462c054b0e1e4d972cf61d8ab6b8cbdc101
Romekxd <149534184+Romeofaiz@users.noreply.github.com>
2024-05-18 01:13:26 +0530

    𝐑𝐎𝐌𝐄𝐊-𝐗𝐃-𝐁𝐎𝐓

---
+++ plugins/intro.js
@@ -0,0 +1,83 @@
+/**
+
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                                   //
+//                                                                                                      // 
+//                                         Ｖ：1．2．2                                                   // 
+//                                                                                                      // 
+//            ███████╗██╗   ██╗██╗  ██╗ █████╗ ██╗██╗         ███╗   ███╗██████╗                        //
+//            ██╔════╝██║   ██║██║  ██║██╔══██╗██║██║         ████╗ ████║██╔══██╗                       //
+//            ███████╗██║   ██║███████║███████║██║██║         ██╔████╔██║██║  ██║                       //
+//            ╚════██║██║   ██║██╔══██║██╔══██║██║██║         ██║╚██╔╝██║██║  ██║                       //
+//            ███████║╚██████╔╝██║  ██║██║  ██║██║███████╗    ██║ ╚═╝ ██║██████╔╝                       //
+//            ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝    ╚═╝     ╚═╝╚═════╝                        //
+//                                                                                                      //
+//                                                                                                      //
+//                                                                                                      //
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+
+CURRENTLY RUNNING ON BETA VERSION!!
+*
+   * @project_name : Suhail-Md
+   * @author : Suhail <https://github.com/SuhailTechInfo>
+   * @youtube : https://www.youtube.com/c/@SuhailTechInfo
+   * @infoription : Suhail-Md ,A Multi-functional whatsapp user bot.
+   * @version 1.2.2 
+*
+   * Licensed under the  GPL-3.0 License;
+* 
+   * ┌┤Created By Suhail Tech Info.
+   * © 2023 Suhail-Md ✭ ⛥.
+   * plugin date : 10/6/2023
+* 
+   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   * SOFTWARE.
+**/
+
+
+
+const { smd, Config,smdBuffer,  prefix } = require('../lib')
+
+
+var surl = 'https://wa.me/919341378016' // Source URL
+const number = '919341378016'
+var name = ' ROMEK XD'
+var body = '𝑅𝚯𝚳𝚵𝐊 𝛨𝛯𝑅𝛯'
+var image = 'https://telegra.ph/file/5358a9249b99cc9192daf.jpg'
+let text = `╭═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄
+│       「 𝗠𝗬 𝗜𝗡𝗧𝗥𝗢 」
+│ Name      : 𝑅𝚯𝚳𝚵𝐊-𝚾𝐃-𝚩𝚯𝚻
+│ Place       :  ᵐᵘᵐᵇᵃⁱ ᴹᵃʰᵃʳᵃˢʰᵗʳᵃ
+│ Gender    :  ᴍᴀʟᴇ
+│ Age          : 19_
+│ Phone     : https://wa.me/919341378016
+│ Youtube   : 
+│ Status     : ɢᴀᴍᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ
+╰═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄`
+
+
+
+ //---------------------------------------------------------------------------
+ smd({
+             pattern: "intro",
+             desc: "Show intro of user",
+             category: "fun",
+             filename: __filename,
+             use: '<group link.>',
+         },
+         async(message) => {
+    try{
+          let media ;try{ media = await smdBuffer(image) }catch{media = log0}
+           const q =await message.bot.fakeMessage("contact",{},name) 
+           let contextInfo = {...(await message.bot.contextInfo(name,body,media,1,surl, 2) )}
+           await message.send(text, {contextInfo : contextInfo },"suhail",  q )
+    }catch(e){ await message.error(`${e}\n\ncommand: intro`,e,false)}
+
+
+ })
